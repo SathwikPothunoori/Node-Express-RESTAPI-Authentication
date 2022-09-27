4c1ac66f74ded82cbf7605b85de21e4445eeca4b
Sathwik Pothunoori <sathwikpothunoori@gmail.com>
2022-09-27 23:56:36 +0530

    cookies

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('1234-5678-91011-1233'));
 
 function auth(req , res , next){
     console.log(req.headers);
-
-    var authHeader = req.headers.authorization;
+    if(!req.signedCookies.user){
+      var authHeader = req.headers.authorization;
     if(!authHeader){
       var err= new Error("You are not authenticated!!")
 
@@ -45,12 +45,13 @@ function auth(req , res , next){
       next(err)
     }
 
-    var  auth = new Buffer(authHeader.split(' ')[1] , 'base64').toString().split(':');
+    var  auth = new Buffer.from(authHeader.split(' ')[1] , 'base64').toString().split(':');
 
     var username = auth[0];
     var password = auth[1];
 
     if(username==='admin' && password==='password'){
+      res.cookie('user','admin',{signed:true})
       next();
     }
     else{
@@ -60,6 +61,18 @@ function auth(req , res , next){
       err.status = 401;
       next(err)
     }
+    }
+    else{
+      if(req.signedCookies.user=='admin'){
+        next()
+      }
+      else{
+      var err= new Error("You are not authenticated!!")
+      err.status = 401;
+      next(err)
+      }
+    }
+    
 }
 
 app.use(auth);
